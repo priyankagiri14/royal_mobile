8da73423b1ed14c8d7a99d7b57a5384e79e0683b
Mohit <mohit95.sikri@gmail.com>
2019-12-06 12:27:09 +0530

    // sync changes

--- app/src/main/java/com/app/mobile/royal/Web_Services/RetrofitClient.java
+++ app/src/main/java/com/app/mobile/royal/Web_Services/RetrofitClient.java
@@ -1,12 +1,16 @@
 package com.app.mobile.royal.Web_Services;
 
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 import static com.app.mobile.royal.Web_Services.IBaseURL.BASE_URL;
+
 
 public class RetrofitClient {
     private static Retrofit retrofit = null;
@@ -15,9 +19,16 @@ public class RetrofitClient {
     {
 
         final OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        // added query param of deviceType
+        Interceptor clientInterceptor = chain -> {
+            Request request = chain.request();
+            HttpUrl url = request.url().newBuilder().addQueryParameter("deviceType", "MOBILE").build();
+            request = request.newBuilder().url(url).build();
+            return chain.proceed(request);
+        };
 
-
-        httpClient .addNetworkInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        httpClient .addNetworkInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        ).addInterceptor(clientInterceptor)
             .build();
         OkHttpClient client = httpClient.build();
         retrofit = new Retrofit.Builder()
--- app/src/main/java/com/app/mobile/royal/Web_Services/RetrofitToken.java
+++ app/src/main/java/com/app/mobile/royal/Web_Services/RetrofitToken.java
@@ -1,10 +1,11 @@
 package com.app.mobile.royal.Web_Services;
 
+import com.app.mobile.royal.Web_Services.Utils.Pref;
+
 import java.io.IOException;
 
-import com.app.mobile.royal.Web_Services.Utils.Pref;
+import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
-
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -15,6 +16,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 import static com.app.mobile.royal.Web_Services.IBaseURL.BASE_URL_GET;
 
+
 public class RetrofitToken {
     public static String token;
     private static Retrofit retrofit = null;
@@ -23,8 +25,13 @@ public class RetrofitToken {
     {
 
         final OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-
-
+        // added query param of deviceType
+        Interceptor clientInterceptor = chain -> {
+            Request request = chain.request();
+            HttpUrl url = request.url().newBuilder().addQueryParameter("deviceType", "MOBILE").build();
+            request = request.newBuilder().url(url).build();
+            return chain.proceed(request);
+        };
         httpClient.addInterceptor(new Interceptor() {
             @Override
             public Response intercept(Interceptor.Chain chain) throws IOException {
@@ -39,7 +46,10 @@ public class RetrofitToken {
         })
                 //here we adding Interceptor for full level logging
                 .addNetworkInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .addInterceptor(clientInterceptor)
                 .build();
+
+
         OkHttpClient client = httpClient.build();
 
         retrofit = new Retrofit.Builder()
